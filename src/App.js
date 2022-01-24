bcba5861168f3ef43db7f6db085808fec2304006
Guillaume Delly <gui7787@gmail.com>
2022-01-24 22:00:28 +0100

    mise à jour 1 du 24/01/2022

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Home from './components/Home';
 import Header from './components/Header';
-import Banner from './components/Banner';
+
 
 function App() {
   return (
     <div className="App">
      <Header></Header>
      <Home></Home>
-     <Banner></Banner>
+    
      
     </div>
   );
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,10 @@ import './Banner.css';
 function Banner() {
     return (
         <div className='banner'>
-            
+            <div className='banner_info'>
+                <h1>Resrvez les meilleurs hôtels au meilleur prix</h1> 
+            <h5>Phrase à trouver</h5>
+            </div>
         </div>
     )
 }
